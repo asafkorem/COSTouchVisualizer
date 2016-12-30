59992972e0ee0073054f01035c67a70a9efeacea
Joe Blau <jblau@uberatc.com>
2016-12-30 01:05:10 -0500

    Fixes #25

--- ExampleSwift/TouchVisualizer/AppDelegate.swift
+++ ExampleSwift/TouchVisualizer/AppDelegate.swift
@@ -12,35 +12,38 @@ import COSTouchVisualizer
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, COSTouchVisualizerWindowDelegate {
 
-    lazy var window: UIWindow? = {
-        var customWindow = COSTouchVisualizerWindow(frame: UIScreen.mainScreen().bounds)
+    internal var window: UIWindow? = {
+        let customWindow = COSTouchVisualizerWindow(frame: UIScreen.main.bounds)
         
-        customWindow.fillColor = UIColor.purpleColor()
-        customWindow.strokeColor = UIColor.blueColor()
+        customWindow.fillColor = UIColor.purple
+        customWindow.strokeColor = UIColor.blue
         customWindow.touchAlpha = 0.4;
         
-        customWindow.rippleFillColor = UIColor.purpleColor()
-        customWindow.rippleStrokeColor = UIColor.blueColor()
+        customWindow.rippleFillColor = UIColor.purple
+        customWindow.rippleStrokeColor = UIColor.blue
         customWindow.touchAlpha = 0.1;
         
-        customWindow.touchVisualizerWindowDelegate = self
         return customWindow
         }()
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        guard let window = window as? COSTouchVisualizerWindow else {
+            return false
+        }
+        window.touchVisualizerWindowDelegate = self
         return true
     }
     
     // MARK: - COSTouchVisualizerWindowDelegate
 
-    func touchVisualizerWindowShouldAlwaysShowFingertip(window: COSTouchVisualizerWindow!) -> Bool {
+    func touchVisualizerWindowShouldAlwaysShowFingertip(_ window: COSTouchVisualizerWindow!) -> Bool {
         // Return YES to make the fingertip always display even if there's no any mirrored screen.
         // Return NO or don't implement this method if you want to keep the fingertip display only when
         // the device is connected to a mirrored screen.
         return true
     }
 
-    func touchVisualizerWindowShouldShowFingertip(window: COSTouchVisualizerWindow!) -> Bool {
+    func touchVisualizerWindowShouldShowFingertip(_ window: COSTouchVisualizerWindow!) -> Bool {
         // Return YES or don't implement this method to make this window show fingertip when necessary.
         // Return NO to make this window not to show fingertip.
         return true
--- ExampleSwift/TouchVisualizer/MasterViewController.swift
+++ ExampleSwift/TouchVisualizer/MasterViewController.swift
@@ -20,9 +20,9 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(MasterViewController.insertNewObject(_:)))
         self.navigationItem.rightBarButtonItem = addButton
     }
 
@@ -31,51 +31,51 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    func insertNewObject(_ sender: AnyObject) {
+        objects.insert(Date() as AnyObject, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
     // MARK: - Segues
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow() {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
                 let object = objects[indexPath.row] as! NSDate
-            (segue.destinationViewController as! DetailViewController).detailItem = object
+            (segue.destination as! DetailViewController).detailItem = object
             }
         }
     }
 
     // MARK: - Table View
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) 
 
-        let object = objects[indexPath.row] as! NSDate
+        let object = objects[indexPath.row] as! Date
         cell.textLabel!.text = object.description
         return cell
     }
 
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
